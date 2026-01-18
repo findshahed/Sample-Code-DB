ed8a17d686f89a0672473cc359062c9ab29b86b7
findshahed <findshahed@gmail.com>
2026-01-19 03:17:08 +0530

    Update versions.tf

--- trade-data-pipeline-snowflake/00 - Infrastructure /versions.tf
+++ trade-data-pipeline-snowflake/00 - Infrastructure /versions.tf
@@ -1,1 +1,35 @@
-
+# infrastructure/versions.tf
+terraform {
+  required_version = ">= 1.0.0"
+  
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.34.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 4.34.0"
+    }
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = ">= 0.40.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.1.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.7.2"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = ">= 2.1.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 3.1.0"
+    }
+  }
+}
