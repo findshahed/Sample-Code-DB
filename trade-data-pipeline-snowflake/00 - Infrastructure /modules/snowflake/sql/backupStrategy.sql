3a31cb34a0844a9cbe9322af9dd9b14fc28d2d17
findshahed <findshahed@gmail.com>
2026-01-19 03:51:12 +0530

    Create backupStrategy.sql

---
+++ trade-data-pipeline-snowflake/00 - Infrastructure /modules/snowflake/sql/backupStrategy.sql
@@ -0,0 +1,15 @@
+-- Create failover group
+CREATE FAILOVER GROUP TRADE_FAILOVER_GROUP
+  OBJECT_TYPES = ACCOUNT, DATABASES, SHARES
+  ALLOWED_DATABASES = (TRADE_DB)
+  ALLOWED_ACCOUNTS = (secondary_account)
+  REPLICATION_SCHEDULE = '10 MINUTE';
+
+-- Enable time travel for all tables
+ALTER DATABASE TRADE_DB 
+SET DATA_RETENTION_TIME_IN_DAYS = 90;
+
+-- Create periodic clones for point-in-time recovery
+CREATE OR REPLACE DATABASE TRADE_DB_DEV_CLONE 
+CLONE TRADE_DB 
+AT (OFFSET => -60*60*24); -- 24 hours ago
