235e7fe5a022a119b24f2fd39886b313ca2f294a
findshahed <findshahed@gmail.com>
2026-01-20 18:37:37 +0530

    Update clusteringKeys.sql

--- trade-data-pipeline-snowflake/00 - Infrastructure /modules/snowflake/sql/clusteringKeys.sql
+++ trade-data-pipeline-snowflake/00 - Infrastructure /modules/snowflake/sql/clusteringKeys.sql
@@ -1,4 +1,7 @@
 -- Optimize clustering for common query patterns
+-- Following script updates clustering keys for table valid trades
+-- It also have a query to monitor cluster efficiency
+
 ALTER TABLE TRADE_DB.PROCESSED.VALID_TRADES 
 CLUSTER BY (
     DATE(TRADE_DATE),
